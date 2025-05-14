c8fb42a685760ef4d3ce74a89b7625ebca58b15c
NadezhdaSinkevich <n.sinkevich@rambler.ru>
2025-05-14 21:48:58 +0300

    1commit

---
+++ src_dir/by/academy/java/lesson1/main.java
@@ -0,0 +1,7 @@
+package by.academy.java.lesson1;
+
+public class main {
+    public static void main(String[] args){
+        System.out.println("Hello lesson");
+    }
+}
---
+++ src_dir/by/academy/java/main.java
@@ -0,0 +1,7 @@
+package by.academy.java;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello main");
+    }
+}
