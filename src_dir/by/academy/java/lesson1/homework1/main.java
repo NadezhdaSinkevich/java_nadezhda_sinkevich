0f1437aebc7860518869046eab32a215c6924769
NadezhdaSinkevich <n.sinkevich@rambler.ru>
2025-05-17 23:22:05 +0300

    commit_4

--- src_dir/by/academy/java/lesson1/homework1/main.java
+++ src_dir/by/academy/java/lesson1/homework1/main.java
@@ -2,6 +2,6 @@ package by.academy.java.lesson1.homework1;
 
 public class main {
         public static void main(String[] args ){
-            System.out.println("message"+3);
+            System.out.println("message"+4);
         }
 }
